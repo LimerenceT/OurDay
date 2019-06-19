556ff0886744c2298f9a712211fbf9fc6b1e0051
LimerenceT <936611560@qq.com>
2019-06-20 04:00:27 +0800

    First commit

---
+++ app/src/main/java/com/day/ourday/ItemListActivity.java
@@ -0,0 +1,267 @@
+package com.day.ourday;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.day.ourday.dummy.DummyContent;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * An activity representing a list of Items. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link ItemDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+public class ItemListActivity extends AppCompatActivity {
+
+    private static final int STORAGE_PERMISSION = 0x20;
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+    private static final int REQUEST_CODE_GALLERY = 0x10;
+    private File imageFile = null;// 声明File对象
+    private static final int CROP_PHOTO = 0x12;// 裁剪图片标识请求码
+
+    private ImageView imageView;
+    private Uri imageUri = null;// 裁剪后的图片uri
+
+    private void gallery() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // 以startActivityForResult的方式启动一个activity用来获取返回的结果
+        startActivityForResult(intent, REQUEST_CODE_GALLERY);
+
+    }
+
+    private void requestStoragePermission() {
+
+        int hasCameraPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int writePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        Log.e("TAG", "开始" + hasCameraPermission);
+        if (hasCameraPermission + writePermission == PackageManager.PERMISSION_GRANTED) {
+            // 拥有权限，可以执行涉及到存储权限的操作
+            Log.e("TAG", "你已经授权了该组权限");
+        } else {
+            // 没有权限，向用户申请该权限
+            Log.e("TAG", "向用户申请该组权限");
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, STORAGE_PERMISSION);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {// 操作成功了
+
+            switch (requestCode) {
+
+                case REQUEST_CODE_GALLERY:// 图库选择图片
+
+                    int height = getWindow().getDecorView().getHeight();
+                    int width = getWindow().getDecorView().getWidth();
+                    Uri uri = data.getData();// 获取图片的uri
+
+                    Intent intent_gallery_crop = new Intent("com.android.camera.action.CROP");
+                    intent_gallery_crop.setDataAndType(uri, "image/*");
+
+                    // 设置裁剪
+                    intent_gallery_crop.putExtra("crop", "true");
+                    intent_gallery_crop.putExtra("scale", true);
+                    // aspectX aspectY 是宽高的比例
+                    intent_gallery_crop.putExtra("aspectX", 3);
+                    intent_gallery_crop.putExtra("aspectY", 4);
+                    // outputX outputY 是裁剪图片宽高
+                    intent_gallery_crop.putExtra("outputX", width);
+                    intent_gallery_crop.putExtra("outputY", height);
+
+                    intent_gallery_crop.putExtra("return-data", false);
+
+                    // 创建文件保存裁剪的图片
+                    createImageFile();
+                    imageUri = Uri.fromFile(imageFile);
+
+                    if (imageUri != null) {
+                        intent_gallery_crop.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                        intent_gallery_crop.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+                    }
+
+                    startActivityForResult(intent_gallery_crop, CROP_PHOTO);
+
+                    break;
+
+                case CROP_PHOTO:// 裁剪图片
+
+                    try {
+
+                        if (imageUri != null) {
+                            displayImage(imageUri);
+                        }
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    break;
+
+            }
+
+        }
+    }
+
+    private void createImageFile() {
+
+        try {
+
+            if (imageFile != null && imageFile.exists()) {
+                imageFile.delete();
+            }
+
+          Log.d("dd",this.getFilesDir().getPath());
+            imageFile = new File(this.getFilesDir().getPath(),System.currentTimeMillis() + "galleryDemo.jpg");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void displayImage(Uri imageUri) throws IOException {
+        Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+        BitmapDrawable bd = new BitmapDrawable(getResources(), bitmap);
+//        imageView.setImageBitmap(bitmap);
+        imageView.setBackground(bd);
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(getTitle());
+        imageView = findViewById(R.id.imageView);
+        requestStoragePermission();
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                gallery();
+            }
+        });
+
+        if (findViewById(R.id.item_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+        }
+
+        View recyclerView = findViewById(R.id.item_list);
+        assert recyclerView != null;
+        setupRecyclerView((RecyclerView) recyclerView);
+    }
+
+
+    private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+        recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(this, DummyContent.ITEMS, mTwoPane));
+    }
+
+    public static class SimpleItemRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
+
+        private final ItemListActivity mParentActivity;
+        private final List<DummyContent.DummyItem> mValues;
+        private final boolean mTwoPane;
+        private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DummyContent.DummyItem item = (DummyContent.DummyItem) view.getTag();
+                if (mTwoPane) {
+                    Bundle arguments = new Bundle();
+                    arguments.putString(ItemDetailFragment.ARG_ITEM_ID, item.id);
+                    ItemDetailFragment fragment = new ItemDetailFragment();
+                    fragment.setArguments(arguments);
+                    mParentActivity.getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.item_detail_container, fragment)
+                            .commit();
+                } else {
+                    Context context = view.getContext();
+                    Intent intent = new Intent(context, ItemDetailActivity.class);
+                    intent.putExtra(ItemDetailFragment.ARG_ITEM_ID, item.id);
+
+                    context.startActivity(intent);
+                }
+            }
+        };
+
+        SimpleItemRecyclerViewAdapter(ItemListActivity parent,
+                                      List<DummyContent.DummyItem> items,
+                                      boolean twoPane) {
+            mValues = items;
+            mParentActivity = parent;
+            mTwoPane = twoPane;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_list_content, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, int position) {
+            holder.mIdView.setText(mValues.get(position).id);
+            holder.mContentView.setText(mValues.get(position).content);
+
+            holder.itemView.setTag(mValues.get(position));
+            holder.itemView.setOnClickListener(mOnClickListener);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.size();
+        }
+
+        class ViewHolder extends RecyclerView.ViewHolder {
+            final TextView mIdView;
+            final TextView mContentView;
+
+            ViewHolder(View view) {
+                super(view);
+                mIdView = view.findViewById(R.id.id_text);
+                mContentView = view.findViewById(R.id.content);
+            }
+        }
+    }
+}
