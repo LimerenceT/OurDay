c1233a4aabcd433336c7c9111664614e4a51bf00
LimerenceT <936611560@qq.com>
2019-07-05 00:00:56 +0800

    Change add item widow animation

--- app/src/main/java/com/day/ourday/MoreWindow.java
+++ app/src/main/java/com/day/ourday/MoreWindow.java
@@ -1,11 +1,8 @@
 package com.day.ourday;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.app.Activity;
-import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Rect;
 import android.graphics.drawable.BitmapDrawable;
@@ -16,7 +13,6 @@ import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
@@ -182,39 +178,9 @@ public class MoreWindow extends PopupWindow implements View.OnClickListener {
      */
     public void showMoreWindow(View anchor) {
         initData();
-
+        setAnimationStyle(R.style.popwin_anim_style);
         showAtLocation(anchor, Gravity.TOP | Gravity.START, 0, 0);
-        mHandler.post(() -> {
-            try {
-                //圆形扩展的动画
-//                int x = mWidth / 2;
-                // int y = (int) (mHeight - fromDpToPx(25));
-
-                int x = mWidth - 15;
-                int y = 15;
-                Animator animator = ViewAnimationUtils.createCircularReveal(bgView, x,
-                        y, 0, bgView.getHeight());
-//                animator.addListener(new AnimatorListenerAdapter() {
-//                    @Override
-//                    public void onAnimationStart(Animator animation) {
-//                                bgView.setVisibility(View.VISIBLE);
-//                    }
-//
-//                    @Override
-//                    public void onAnimationEnd(Animator animation) {
-//                        							layout.setVisibility(View.VISIBLE);
-//                    }
-//                });
-                animator.setDuration(100);
-                animator.start();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        });
-
-
-        showAnimation(layout);
-
+//        showAnimation(layout);
     }
 
     private void initData() {
@@ -223,6 +189,7 @@ public class MoreWindow extends PopupWindow implements View.OnClickListener {
 
     }
 
+    // 容器中的控件动画
     private void showAnimation(ViewGroup layout) {
         try {
             LinearLayout linearLayout = layout.findViewById(R.id.line);
@@ -258,37 +225,7 @@ public class MoreWindow extends PopupWindow implements View.OnClickListener {
      */
     // FIXME: 2019-06-29 能够连续点两次
     private void closeAnimation() {
-        mHandler.post(() -> close.animate().rotation(-90).setDuration(400));
-
-        try {
-
-//            int x = mWidth / 2;
-//            int y = (int) (mHeight - fromDpToPx(25));
-            int x = mWidth - 15;
-            int y = 15;
-            Animator animator = ViewAnimationUtils.createCircularReveal(bgView, x,
-                    y, bgView.getHeight(), 0);
-            animator.addListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationStart(Animator animation) {
-//                    layout.setVisibility(View.GONE);
-                }
-
-                @Override
-                public void onAnimationEnd(Animator animation) {
-//                    bgView.setVisibility(View.GONE);
-                    dismiss();
-                }
-            });
-            animator.setDuration(300);
-            animator.start();
-        } catch (Exception e) {
-        }
-    }
-
-
-    float fromDpToPx(float dp) {
-        return dp * Resources.getSystem().getDisplayMetrics().density;
+        dismiss();
     }
 
     @Override
